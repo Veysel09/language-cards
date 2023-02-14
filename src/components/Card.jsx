7d6706f78c46b22a199227e2d69a2739ecc59912
Veysel Aydin <weycell.aydin09@gmail.com>
2023-02-14 17:00:44 +0300

    data

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,22 @@
-import  Container  from "react-bootstrap/Container";
 import Language from "./Language";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import { data } from "../helpers/data";
 
 const Card = () => {
   return (
-    <Container>
+    <Container className="rounded-5 mt-4" style={{ background: "#96775a" }}>
       <h1>Language</h1>
-      <Language />
+      <Row>
+        {data.map((lang, index) => {
+          return (
+            <Col>
+              <Language {...lang} key={index} />
+            </Col>
+          );
+        })}
+      </Row>
     </Container>
   );
 };
