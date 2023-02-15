61ebe1fa6874a1d256db359092c55819e8d5e160
Veysel Aydin <weycell.aydin09@gmail.com>
2023-02-15 14:01:41 +0300

    showImage

--- src/components/Language.jsx
+++ src/components/Language.jsx
@@ -3,23 +3,27 @@ import Container from "react-bootstrap/Container";
 import Image from "react-bootstrap/Image";
 
 const Language = ({ name, img, options }) => {
-  const [showDesc, setShowDesc] = useState(false);
+  const [showImage, setShowImage] = useState(true);
   // console.log(showDesc);
   return (
     <Container
       style={{ background: "peachpuff" }}
       className="p-4 rounded-3"
-      onClick={() => setShowDesc(!showDesc)}
+      onClick={() => setShowImage(!showImage)}
     >
-      <Container>
-        <Image src={img} width="70%"></Image>
-        <h3 className="display-6">{name}</h3>
-      </Container>
-      <ol>
-        {options.map((item) => {
-          return <li>{item}</li>;
-        })}
-      </ol>
+      {showImage && (
+        <Container>
+          <Image src={img} width="70%"></Image>
+          <h3 className="display-6">{name}</h3>
+        </Container>
+      )}
+      {!showImage && (
+        <ol>
+          {options.map((item) => {
+            return <li className="h5 text-start">{item}</li>;
+          })}
+        </ol>
+      )}
     </Container>
   );
 };
